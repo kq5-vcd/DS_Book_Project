b88026c4275f4926e51c8cf08cf3203a1ae0199a
Hong Quan <quandh619@gmail.com>
2020-12-14 01:10:39 +0700

    Add genre scraping

---
+++ goodreads/goodreads/spiders/genre_scraper.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+@author: quan.dh176850
+"""
+
+import scrapy
+import pandas as pd
+from itertools import cycle
+
+
+class Genre(scrapy.Spider):
+    name = "genre"
+    
+    bookdb = pd.read_csv("books.csv")
+    bookdb = bookdb[bookdb.GenreLink.notnull()]
+    
+    bookID = cycle(bookdb["BookID"])
+    nextID = next(bookID)
+    genreLink = bookdb["GenreLink"]
+    
+    start_urls = ["https://www.goodreads.com{}".format(link) for link in genreLink]
+    
+    def parse(self, response):
+        genre = ""
+        for res in response.xpath('//div[@class="shelfStat"]'):
+            genre = res.xpath('div/a/text()').get()
+            
+            if genre is not None:
+                if self.hasNumber(genre) == False:
+                    yield {
+                        'BookID' : self.nextID,
+                        'Genre' : genre
+                        }
+        self.nextID = next(self.bookID)
+        
+        
+    def hasNumber(self,string):
+        return any(char.isdigit() for char in string)
+    
+        
