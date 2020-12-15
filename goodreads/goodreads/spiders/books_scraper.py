d20aed4df81699a8e7d20b0b723807c6e47431c2
DuyCVu <duy822.kq5@gmail.com>
2020-12-16 02:57:48 +0700

    The first 100k rows

--- goodreads/goodreads/spiders/books_scraper.py
+++ goodreads/goodreads/spiders/books_scraper.py
@@ -11,8 +11,8 @@ from dateparser.search import search_dates
 class Publish(scrapy.Spider):
     name = "books"
 
-    start_id = 105001
-    end_id = 115001
+    start_id = 35001
+    end_id = 100001
     amount = list(range(start_id,end_id))
     start_urls = ["https://www.goodreads.com/book/show/{}".format(i) for i in amount]
 
