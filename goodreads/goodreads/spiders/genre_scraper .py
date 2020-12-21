5761dab54b22033a7f842428ace8593fd7723916
Hong Quan <quandh619@gmail.com>
2020-12-22 00:49:41 +0700

    recrawl genre

---
+++ goodreads/goodreads/spiders/genre_scraper .py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+@author: quan.dh176850
+"""
+
+import scrapy
+import pandas as pd
+from itertools import cycle
+
+
+class Genre(scrapy.Spider):
+    name = "genre"
+    
+    bookdb = pd.read_csv("data_q.csv")
+    genreLink = bookdb["GenreLink"]
+    
+    start_urls = ["https://www.goodreads.com{}".format(link) for link in genreLink]
+    
+    def parse(self, response):
+        
+        genre = ""
+        num_people = 0
+        for res in response.xpath('//div[@class="shelfStat"]'):
+            genre = res.xpath('div[@style="float: left; width: 100px;"]/a/text()').get()
+            people = res.xpath('div[@class="smallText"]/a/text()').get()
+            num = people.split()
+            people = num[0]
+            if people.find(",") != -1:
+                people = people.replace(",","")
+            num_people = int(people)
+            
+            
+            if genre is not None:
+                if self.hasNumber(genre) == False:
+                    yield {
+                        'GenreLink' : response.request.url[25:],
+                        'Genre' : genre,
+                        'NumberOfPeople' : num_people
+                        }
+        
+        
+        
+    def hasNumber(self,string):
+        return any(char.isdigit() for char in string)
+    
+        
