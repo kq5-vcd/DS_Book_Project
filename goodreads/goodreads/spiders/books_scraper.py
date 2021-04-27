76b8ea06fb84285df7a5dfb3a277f66ca4f29910
BysGits <quandh619@gmail.com>
2021-04-28 01:55:06 +0700

    save

--- goodreads/goodreads/spiders/books_scraper.py
+++ goodreads/goodreads/spiders/books_scraper.py
@@ -10,8 +10,8 @@ from dateparser.search import search_dates
 class Publish(scrapy.Spider):
     name = "books"
 
-    start_id = 12250001
-    end_id = 12300001
+    start_id = 12450001
+    end_id = 12500001
 
 
 
