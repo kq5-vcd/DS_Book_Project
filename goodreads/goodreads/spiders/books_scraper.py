e85027955bcc9d0a8668d4b7c0f439ebdf5dbd88
BysGits <quandh619@gmail.com>
2021-05-31 09:24:55 +0700

    mutex

--- goodreads/goodreads/spiders/books_scraper.py
+++ goodreads/goodreads/spiders/books_scraper.py
@@ -10,8 +10,8 @@ from dateparser.search import search_dates
 class Publish(scrapy.Spider):
     name = "books"
 
-    start_id = 12850001
-    end_id = 12900001
+    start_id = 12900001
+    end_id = 12950001
 
 
 
